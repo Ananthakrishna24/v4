0aba61882e95b0b0946c44aa74f389651d9e50d1
AnanthaKrishna <ananthaak24@gmail.com>
2021-06-30 11:53:27 +0530

    logo Update

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -142,7 +142,7 @@ const About = () => {
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
-      <h2 className="numbered-heading">About Me</h2>
+      <h1 className="numbered-heading">About Me</h1>
 
       <div className="inner">
         <StyledText>
