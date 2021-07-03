a5833cc04387e365f649f4e3f8534dedcf8a605e
AnanthaKrishna <ananthaak24@gmail.com>
2021-07-03 20:05:05 +0530

    major

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Ananthakrishna',
     description:
       'Ananthakrishna is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: '', // No trailing slash allowed!
+    siteUrl: 'ananthakrishna.netlify.app', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@Anantha_aka',
   },
