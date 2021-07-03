e0c8767e8c7e1ada2edc9bfa280f010f8db79802
AnanthaKrishna <ananthaak24@gmail.com>
2021-07-03 13:41:07 +0530

    project update

--- src/pages/404.js
+++ src/pages/404.js
@@ -43,7 +43,7 @@ const NotFoundPage = ({ location }) => {
   const content = (
     <StyledMainContainer className="fillHeight">
       <StyledTitle>404</StyledTitle>
-      <StyledSubtitle>Page Not Found</StyledSubtitle>
+      <StyledSubtitle>Sorry Page Under Development</StyledSubtitle>
       <StyledHomeButton to="/">Go Home</StyledHomeButton>
     </StyledMainContainer>
   );
